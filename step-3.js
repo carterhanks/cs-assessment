c515fea973cbdfc9a588b72e294c3efca9d299ff
carterhanks <carterhanks@gmail.com>
2021-07-02 16:02:13 -0600

    Added solution from EOD

--- step-3.js
+++ step-3.js
@@ -21,10 +21,10 @@ function isPangram(str) {
 
     for (let i = 0; i < alphabet.length; i++) {
         if(lowercase.indexOf(alphabet[i]) === -1) {
-            return 'false';
+            return false;
         };
     };
-    return 'true';
+    return true;
 };
 console.time('isPangram2 timer');
 isPangram('The quick brown fox jumps over the lazy dog.'); //true
@@ -32,3 +32,25 @@ console.timeEnd('isPangram2 timer'); //isPangram2 timer: 0.019ms
 
 // isPangram('I love to code!'); //false
 // isPangram('abcdefghijklmnopqrstuvwxyz'); //true
+
+// ADD TO ZERO
+
+function addToZero(numArr) {
+    if (numArr.length < 2) {
+        return false;
+    };
+    for(let i = 0; i < numArr.length; i++) { // use this loop to have 'i' represent the current number we are evaluating
+        for(let j = i + 1; j < numArr.length; j++) { // use this loop to evaluate only the numbers that come after our numArr[i] element
+            console.log('i =' + i,  'j =' + j)
+
+            if(numArr[i] + numArr[j] === 0){
+                return true;
+            };
+        };
+    };
+    return false;
+};
+
+addToZero([1, 2, 3, -2]);
+
+// O(n log n)
